2280b5c8b46b479d2d7e339bf23f558b6c02503f
beta_chelsea <beta.chelsea@gmail.com>
2023-05-06 23:56:46 +0900

    change usage assert_equal

--- test/test.rb
+++ test/test.rb
@@ -34,29 +34,29 @@ class TestReversi < Minitest::Test
 
   def test_put_stone
     board = build_board('pattern00.txt')
-    assert_equal put_stone!(board, 'e3', BLACK_STONE), true
-    assert_equal build_board('pattern00_step1.txt'), board
-    assert_equal put_stone!(board, 'f5', WHITE_STONE), true
-    assert_equal build_board('pattern00_step2.txt'), board
+    assert_equal true, put_stone!(board, 'e3', BLACK_STONE)
+    assert_equal board, build_board('pattern00_step1.txt')
+    assert_equal true, put_stone!(board, 'f5', WHITE_STONE)
+    assert_equal board, build_board('pattern00_step2.txt')
   end
 
   def test_cannot_put_stone
     board = build_board('pattern01.txt')
-    assert_equal put_stone!(board, 'b8', BLACK_STONE), false
+    assert_equal false, put_stone!(board, 'b8', BLACK_STONE)
   end
 
   def test_turn
     board = build_board('pattern02.txt')
-    assert_equal put_stone!(board, 'b5', BLACK_STONE), true
-    assert_equal build_board('pattern02_after.txt'), board
+    assert_equal true, put_stone!(board, 'b5', BLACK_STONE)
+    assert_equal board, build_board('pattern02_after.txt')
   end
 
   def test_finished?
-    assert_equal finished?(build_board('pattern00.txt')), false # 初期盤面
-    assert_equal finished?(build_board('pattern03a.txt')), true # 全て埋まった
-    assert_equal finished?(build_board('pattern03b.txt')), true # 白最短勝利
-    assert_equal finished?(build_board('pattern03c.txt')), true # 黒最短勝利
-    assert_equal finished?(build_board('pattern03d.txt')), false # 白配置可・黒配置不可
-    assert_equal finished?(build_board('pattern03e.txt')), false # 白配置不可・黒配置可
+    assert_equal false, finished?(build_board('pattern00.txt'))  # 初期盤面
+    assert_equal true,  finished?(build_board('pattern03a.txt')) # 全て埋まった
+    assert_equal true,  finished?(build_board('pattern03b.txt')) # 白最短勝利
+    assert_equal true,  finished?(build_board('pattern03c.txt')) # 黒最短勝利
+    assert_equal false, finished?(build_board('pattern03d.txt')) # 白配置可・黒配置不可
+    assert_equal false, finished?(build_board('pattern03e.txt')) # 白配置不可・黒配置可
   end
 end
