21c76849ae3fe040940f02bbed7068bd063131ab
Ryo <131308883+Ryo-ta090972@users.noreply.github.com>
2023-09-14 15:16:42 +0900

    Revert "Debug"

--- lib/reversi_methods.rb
+++ lib/reversi_methods.rb
@@ -47,7 +47,7 @@ module ReversiMethods
 
     # コピーした盤面にて石の配置を試みて、成功すれば反映する
     copied_board = Marshal.load(Marshal.dump(board))
-    copied_board[pos.row][pos.col] = stone_color
+    copied_board[pos.col][pos.row] = stone_color
 
     turn_succeed = false
     Position::DIRECTIONS.each do |direction|
@@ -63,7 +63,6 @@ module ReversiMethods
   def turn(board, target_pos, attack_stone_color, direction)
     return false if target_pos.out_of_board?
     return false if target_pos.stone_color(board) == attack_stone_color
-    return false if target_pos.stone_color(board) == BLANK_CELL
 
     next_pos = target_pos.next_position(direction)
     if (next_pos.stone_color(board) == attack_stone_color) || turn(board, next_pos, attack_stone_color, direction)
@@ -84,10 +83,9 @@ module ReversiMethods
         next unless cell == BLANK_CELL
 
         position = Position.new(row, col)
-        return true if put_stone(board, position.to_cell_ref.chars, attack_stone_color, dry_run: true)
+        return true if put_stone(board, position.to_cell_ref, attack_stone_color, dry_run: true)
       end
     end
-    return false
   end
 
   def count_stone(board, stone_color)
